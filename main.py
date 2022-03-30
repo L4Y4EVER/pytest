8740127a385130b9f79122a7f10782a31527c756
superLee <m13943206817@163.com>
2022-03-30 19:26:34 +0800

    Update main.py
    修改日志清洗代码

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 # This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import json
 import os
 import time
 
 import pandas as pd
 import datetime as dt
-import numpy as np
+# import numpy as np
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+# 获取有关发消息之间差值方法
 def make_data():
     for info in os.listdir('D:/downloads/csv'):
         domain = os.path.abspath('D:/downloads/csv')
@@ -39,6 +38,7 @@ def make_data():
                 continue
         print(max - min)
 
+# 获取有关发消息各个指标数据
 def get_data():
     f = open('D:/work/log/downloaded_data.txt',encoding='UTF-8')
     hjbp = set()
@@ -47,11 +47,39 @@ def get_data():
     pchat = set()
     cyj = set()
 
+    date = []
+    bplist = []
+    sjlist = []
+    tmlist = []
+    plist = []
+    cyjlist = []
+
+    t = "2022-03-23"
     # '2022-03-23_08:44:10.165 RCV from:tl_12|0|120036780@im.jia.changyou.com/CYJ'
     for line in f:
         jSon = json.loads(line)
         content = jSon['content']
-        time.strftime('%Y-%m-%d', time.localtime(int(jSon['__time__'])))
+        nt = time.strftime('%Y-%m-%d', time.localtime(int(jSon['__time__'])))
+        if not(t.__eq__(nt)):
+            print("now data:",t)
+            print("hjbp:", len(hjbp))
+            print("hjsj:", len(hjsj))
+            print("hjtm:", len(hjtm))
+            print("pchat:", len(pchat))
+            print("cyj:", len(cyj))
+            date.append(t)
+            bplist.append(len(hjbp))
+            sjlist.append(len(hjsj))
+            tmlist.append(len(hjtm))
+            plist.append(len(pchat))
+            cyjlist.append(len(cyj))
+            t = nt
+            hjbp.clear()
+            hjsj.clear()
+            hjtm.clear()
+            pchat.clear()
+            cyj.clear()
+
         # time.strftime()
         if content.__contains__('RCV from') and content.__contains__('|0|'):
             ujid = str.split(str.split(content, ";")[2], ":")[1]
@@ -70,20 +98,26 @@ def get_data():
             pchat.add(str.split(str.split(content, ";")[0], "from:")[1])
         else:
             continue
+    print("end------date",t)
+    print("hjbp:", len(hjbp))
+    print("hjsj:", len(hjsj))
+    print("hjtm:", len(hjtm))
+    print("pchat:", len(pchat))
+    print("cyj:", len(cyj))
+    date.append(t)
+    bplist.append(len(hjbp))
+    sjlist.append(len(hjsj))
+    tmlist.append(len(hjtm))
+    plist.append(len(pchat))
+    cyjlist.append(len(cyj))
+    # 字典中的key值即为csv中列名 输出到csv
+    dataframe = pd.DataFrame({'date': date, 'bpcount': bplist, 'sjcount': sjlist, 'tmcount': tmlist, 'pccount': plist, 'cyjcount': cyjlist})
+    dataframe.to_csv("D:/work/log/test.csv", index=False, sep=',')
 
-    print("hjbp:", len(hjbp),hjbp)
-    print("hjsj:", len(hjsj),hjsj)
-    print("hjtm:", len(hjtm),hjtm)
-    print("pchat:", len(pchat),pchat)
-    print("cyj:", len(cyj), cyj)
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     get_data()
-    # f = open('D:/work/log/downloaded_data.txt',encoding='UTF-8')
 
 
 
 
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
